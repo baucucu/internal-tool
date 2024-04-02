a8d4dd194b822b0b2d34c56d8e50dc57f9fc9ee1
Alex Raduca <alexandru.raduca@gmail.com>
2024-04-02 05:19:26 +0300

    rebuild

--- components/component/component/features-section.tsx
+++ components/component/component/features-section.tsx
@@ -5,14 +5,6 @@
 
 import { JSX, SVGProps } from "react";
 
-import { JSX, SVGProps } from "react";
-
-import { JSX, SVGProps } from "react";
-
-import { JSX, SVGProps } from "react";
-
-import { JSX, SVGProps } from "react";
-
 export function FeaturesSection() {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-black">
